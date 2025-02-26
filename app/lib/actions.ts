63ffe9be41f94a8ae525dd8cf80e9119f4d6e484
Joseph Stone <stonepreston05@gmail.com>
2025-02-25 20:16:49 -0700

    completed unfinished auth funcitons

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,6 +2,7 @@
 
 import { sql } from "@vercel/postgres";
 import { Blog, HeaderData } from "./definitions";
+import bcrypt from 'bcrypt'
 
 export async function getHeaderData(): Promise<HeaderData[]> {
   try {
@@ -38,3 +39,24 @@ export async function getBlog(id: number) {
     return []
   }
 }
+
+export async function addUser(name:string, email: string, password: string) {
+  const saltRounds = 10
+  const hashedPassword = await bcrypt.hash(password, saltRounds)
+  try {
+      await sql`INSERT INTO users (name, email, password) VALUES (${name}, ${email}, ${hashedPassword})`
+  } catch (error) {
+      console.error(error)
+  }
+}
+
+export async function getUserByUsername(username: string) {
+  try {
+    const user =
+      await sql`SELECT * FROM users WHERE username = ${username}`;
+    return user;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
